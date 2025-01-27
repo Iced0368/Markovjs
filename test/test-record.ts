34f483be6a6b119795f9785035171d0e3581f6ee
Iced0368 <iced03682@gmail.com>
2025-01-27 09:44:35 +0900

    fix testcode: missing transition

--- test/test-record.ts
+++ test/test-record.ts
@@ -8,6 +8,7 @@ const markovTransitions = new MarkovTransitions(
         [["level-0", "level-0"], 0.5], [["level-0", "level-1"], 0.4], [["level-0", "level-2"], 0.1],
         [["level-1", "level-1"], 0.6], [["level-1", "level-2"], 0.3], [["level-1", "level-3"], 0.1],
         [["level-2", "level-2"], 0.7], [["level-2", "level-3"], 0.3],
+        [["level-3", "level-3"], 1.0],
     ]
 )
 
